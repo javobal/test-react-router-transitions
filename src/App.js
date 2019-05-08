dcfe7bc42eab7a1800caaef3a75f9de63577961f
Javier Balam <javier.balam@gmail.com>
2019-05-08 18:42:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
 
-function App() {
+function Index() {
+  return <div className="page">Home</div>;
+}
+
+function About() {
+  return <div className="page">About</div>;
+}
+
+function Users() {
+  return <div className="page">Users</div>;
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <ul className="header">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/users">Users</Link>
+      </li>
+    </ul>
+  );
+}
