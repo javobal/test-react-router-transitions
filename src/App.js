9732935a4274ce7a5061d44f202240cfbf90f9ff
Javier Balam <javier.balam@gmail.com>
2019-05-08 21:02:14 -0500

    Transitions implemented

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
 
-function Index() {
-  return <div className="page">Home</div>;
-}
-
-function About() {
-  return <div className="page">About</div>;
-}
-
-function Users() {
-  return <div className="page">Users</div>;
-}
-
 export default function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route path="/" exact component={Index} />
-          <Route path="/about/" component={About} />
-          <Route path="/users/" component={Users} />
-        </Switch>
+        <Route render={({ location }) => (
+        <TransitionGroup component={null}>
+          <CSSTransition key={location.key} timeout={1000} classNames='fade'>
+            <Switch location={location}>
+              <Route path="/" exact component={Index} />
+              <Route path="/about/" component={About} />
+              <Route path="/users/" component={Users} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+        )} />
       </div>
     </Router>
   );
+}
+
+function Index() {
+  return <div className="page" style={{ background: 'white'}}>Home</div>;
+}
+
+function About() {
+  return <div className="page" style={{ background: 'red'}}>About</div>;
+}
+
+function Users() {
+  return <div className="page" style={{ background: 'blue'}}>Users</div>;
 }
 
 function Header() {
